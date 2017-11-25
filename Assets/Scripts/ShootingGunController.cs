4e400ce244dfef2b67e315b2080084b7e8c75066
4419-2-6\user <squirtle86224@gmail.com>
2017-11-25 09:39:13 +0800

    射線完成

--- Assets/Scripts/ShootingGunController.cs
+++ Assets/Scripts/ShootingGunController.cs
@@ -5,5 +5,49 @@ using VRStandardAssets.Utils;
 using UnityEngine.VR;
 public class ShootingGunController : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public VRInput vrInput;//需先在上面標註使用VRStandardAssets.Utils; 才能用VRInput
+    public ParticleSystem flareParticle;
+    public LineRenderer gunFlare;
+    public Transform gunEnd;
+    public float defaultLineLength= 70f;
+    public float gunFlareVisibleSeconds = 0.07f;
+    private void OnEnable()
+    {
+        vrInput.OnDown += HandleDown;
+    }
+    private void OnDisable()
+    {
+        vrInput.OnDown -= HandleDown;
+    }
 
+    private void HandleDown()
+    {
+        
+        StartCoroutine(Fire());//可先跳過往下走
+        
+    }
+
+    private IEnumerator Fire()
+    {
+        audioSource.Play();
+        float lineLength = defaultLineLength;
+
+        flareParticle.Play();
+        gunFlare.enabled = true;
+        yield return StartCoroutine(MoveLineRenderer(lineLength));//先暫停此方法先等到某步驟完成
+        gunFlare.enabled = false;
+    }
+
+    private IEnumerator MoveLineRenderer(float lineLength)
+    {
+        float timer = 0f;
+        while (timer < gunFlareVisibleSeconds)
+        {
+            gunFlare.SetPosition(0, gunEnd.position);
+            gunFlare.SetPosition(1, gunEnd.position+ gunEnd.forward * lineLength);
+            yield return null;
+            timer += Time.deltaTime;
+        }
+    }
 }
