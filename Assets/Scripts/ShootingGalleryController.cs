39190241f8fce2eb18c99e7940338a5d00f8b316
4419-2-6\user <squirtle86224@gmail.com>
2017-11-25 11:59:43 +0800

    timebar和開始重新game完成

--- Assets/Scripts/ShootingGalleryController.cs
+++ Assets/Scripts/ShootingGalleryController.cs
@@ -4,7 +4,67 @@ using UnityEngine;
 using VRStandardAssets.Common;
 using VRStandardAssets.Utils;
 using UnityEngine.UI;
-public class ShootingGalleryController : MonoBehaviour
+public class ShootingGalleryController : MonoBehaviour //類別
 {
+    public UIController uiController;//欄位
+    public Reticle reticle;
+    public SelectionRadial selectionRadial;
+    public SelectionSlider selectionSlider;
 
+    public Image timerBar;
+    public float gameDuration = 30f;
+    public float endDelay = 1.5f;
+    public bool IsPlaying//property屬性
+    {
+        private set;
+        get;
+    }
+
+    private IEnumerator Start()//Method方法
+    {
+        SessionData.SetGameType(SessionData.GameType.SHOOTER180);
+        while (true)
+        {
+            Debug.Log("Start StartPhase");
+            yield return StartCoroutine(StartPhase());//yield=先去執行()內的方法，執行完後才可以繼續進行下一行
+            Debug.Log("Complete StartPhase");
+            yield return StartCoroutine(PlayerPhase());
+            yield return StartCoroutine(EndPhase());
+            Debug.Log("Complete");
+        }
+    }
+    private IEnumerator StartPhase()
+    {
+        yield return StartCoroutine(uiController.ShowIntroUI());
+        reticle.Show();
+        selectionRadial.Hide();
+        yield return StartCoroutine(selectionSlider.WaitForBarToFill());
+        yield return StartCoroutine(uiController.HideIntroUI());
+    }
+    private IEnumerator PlayerPhase()
+    {
+        yield return StartCoroutine(uiController.ShowPlayerUI());
+        IsPlaying = true;
+        reticle.Show();
+        SessionData.Restart();
+        float gameTimer = gameDuration;
+        while (gameTimer > 0f)
+        {
+            yield return null;
+            gameTimer -= Time.deltaTime;
+            timerBar.fillAmount = gameTimer / gameDuration;
+        }
+        
+        IsPlaying = false;
+        yield return StartCoroutine(uiController.HidePlayerUI());
+    }
+
+    private IEnumerator EndPhase()
+    {
+        reticle.Hide();
+        yield return StartCoroutine (uiController.ShowOutroUI());
+        yield return new WaitForSeconds (endDelay);
+        yield return StartCoroutine (selectionRadial.WaitForSelectionRadialToFill());
+        yield return StartCoroutine(uiController.HideOutroUI());
+    }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -6,5 +6,38 @@ using VRStandardAssets.Common;
 using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
+    public UIFader introUI;
+    public UIFader outroUI;
+    public UIFader playerUI;
+    public Text totalscore;
+    public Text highscore;
 
+    public IEnumerator ShowIntroUI()
+    {
+        yield return StartCoroutine(introUI.InteruptAndFadeIn());
+    }
+    public IEnumerator HideIntroUI()
+    {
+        yield return StartCoroutine(introUI.InteruptAndFadeOut());
+    }
+
+    public IEnumerator ShowOutroUI()
+    {
+        totalscore.text = SessionData.Score.ToString();
+        highscore.text = SessionData.HighScore.ToString();
+        yield return StartCoroutine(outroUI.InteruptAndFadeIn());
+    }
+    public IEnumerator HideOutroUI()
+    {
+        yield return StartCoroutine(outroUI.InteruptAndFadeOut());
+    }
+    public IEnumerator ShowPlayerUI()
+    {
+        yield return StartCoroutine(playerUI.InteruptAndFadeIn());
+    }
+    public IEnumerator HidePlayerUI()
+    {
+        yield return StartCoroutine(playerUI.InteruptAndFadeOut());
+    }
+    
 }
